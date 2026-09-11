db02c7224871e6dd44ca68d338ecd3b56c529cb5
h <hajar.kian@yahoo.com>
2026-09-11 13:51:32 +0430

    v4

--- _next/static/_aO4P1GLcFR4IgXaG_X-8/_buildManifest.js
+++ _next/static/_aO4P1GLcFR4IgXaG_X-8/_buildManifest.js
@@ -2,30 +2,30 @@ self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [
       {
-        "source": "/shopHost/product/:path*/js/:path*",
+        "source": "/shopHost/shopHost/product/:path*/js/:path*",
         "destination": "/shopHost/shopHost/js/:path*"
       },
       {
-        "source": "/shopHost/product/:path*/images/:path*",
+        "source": "/shopHost/shopHost/product/:path*/images/:path*",
         "destination": "/shopHost/shopHost/images/:path*"
       },
       {
-        "source": "/shopHost/product/_next/data/:path*"
+        "source": "/shopHost/shopHost/product/_next/data/:path*"
       },
       {
-        "source": "/shopHost/product/_next/:path*"
+        "source": "/shopHost/shopHost/product/_next/:path*"
       },
       {
-        "source": "/shopHost/product/:path*"
+        "source": "/shopHost/shopHost/product/:path*"
       },
       {
-        "source": "/shopHost/checkout/_next/data/:path*"
+        "source": "/shopHost/shopHost/checkout/_next/data/:path*"
       },
       {
-        "source": "/shopHost/checkout/_next/:path*"
+        "source": "/shopHost/shopHost/checkout/_next/:path*"
       },
       {
-        "source": "/shopHost/checkout/:path*"
+        "source": "/shopHost/shopHost/checkout/:path*"
       }
     ],
     "beforeFiles": [

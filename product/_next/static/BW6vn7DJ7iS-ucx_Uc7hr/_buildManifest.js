b703d0e9ac816d98d08ea1eefe0333722d1a6939
h <hajar.kian@yahoo.com>
2026-09-12 13:21:01 +0430

    add product

---
+++ product/_next/static/BW6vn7DJ7iS-ucx_Uc7hr/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/product"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

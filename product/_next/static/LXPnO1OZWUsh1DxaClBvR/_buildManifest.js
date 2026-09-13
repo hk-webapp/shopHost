c09954b66e4e02bb0a7ded9ed6c91b905a663568
h <hajar.kian@yahoo.com>
2026-09-13 13:39:42 +0430

    ss

--- product/_next/static/LXPnO1OZWUsh1DxaClBvR/_buildManifest.js
+++ product/_next/static/LXPnO1OZWUsh1DxaClBvR/_buildManifest.js
@@ -2,13 +2,13 @@ self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [
       {
-        "source": "/product"
+        "source": "/shopHost/product"
       }
     ],
     "beforeFiles": [
       {
-        "source": "/product//_next/:path+",
-        "destination": "/product/_next/:path+"
+        "source": "/shopHost/product//_next/:path+",
+        "destination": "/shopHost/product/_next/:path+"
       }
     ],
     "fallback": []
